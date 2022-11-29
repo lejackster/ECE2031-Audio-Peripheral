f55256a7c63552ea4080ead84ad5cab434bc54ba
VrajP03 <117402020+VrajP03@users.noreply.github.com>
2022-11-28 19:25:51 -0500

    Update TONE_GEN.vhd

--- TONE_GEN.vhd
+++ TONE_GEN.vhd
@@ -23,7 +23,6 @@ ENTITY TONE_GEN IS
 		-- added
 		KEY3       : IN    STD_LOGIC;
 		KEY2       : IN    STD_LOGIC;
-		KEY1       : IN    STD_LOGIC;
       -- buttons
 		
       L_DATA     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -37,13 +36,23 @@ ARCHITECTURE gen OF TONE_GEN IS
       playingNote,
       notPlayingNote
    );
+	
+	Type volume is (
+		default,
+		volumeUpOne,
+		volumeUpTwo,
+		volumeUpThree,
+		volumeDownOne,
+		volumeDownTwo,
+		volumeDownThree
+	);
 
    SIGNAL phase_register : STD_LOGIC_VECTOR(14 DOWNTO 0);
    SIGNAL tuning_word    : STD_LOGIC_VECTOR(11 DOWNTO 0);
    SIGNAL sounddata      : STD_LOGIC_VECTOR(11 DOWNTO 0);
    SIGNAL playing        : playingStatus;
    SHARED VARIABLE scale_factor : INTEGER :=1;
-   
+   SIGNAL volume_current			: volume;
    
 BEGIN
 
@@ -71,10 +80,96 @@ BEGIN
    
    -- 12-bit sound data is used as bits 12-3 of the 16-bit output.
    -- This is to prevent the output from being too loud.
-	PROCESS (SAMPLE_CLK) Begin
-		IF (RISING_EDGE(CS)) THEN
-			
-		END IF;
+	
+
+	
+	PROCESS (RESETN) BEGIN
+		IF(RESETN='0') THEN
+			L_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+					L_DATA(12 DOWNTO 1) <= sounddata;
+					L_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+					
+					-- Right channel is the same.
+					R_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+					R_DATA(12 DOWNTO 1) <= sounddata;
+					R_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+		Else
+		
+			CASE scale_factor is
+							
+				WHEN 8 =>
+						L_DATA(15 DOWNTO 4) <= sounddata(11 DOWNTO 0);
+						L_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 4) <= sounddata(11 DOWNTO 0);
+						R_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
+					
+				WHEN 4 =>
+						L_DATA(15) <= sounddata(11); -- sign extend
+						L_DATA(14 DOWNTO 3) <= sounddata(11 DOWNTO 0);
+						L_DATA(2 DOWNTO 0) <= "000"; -- pad right side with 0s
+						
+						-- Right channel is the same.
+						R_DATA(15) <= sounddata(11); -- sign extend
+						R_DATA(14 DOWNTO 3) <= sounddata(11 DOWNTO 0);
+						R_DATA(2 DOWNTO 0) <= "000"; -- pad right side with 0s
+						
+				WHEN 2 =>
+						L_DATA(15 DOWNTO 14) <= sounddata(11)&sounddata(11); -- sign extend
+						L_DATA(13 DOWNTO 2) <= sounddata(11 DOWNTO 0);
+						L_DATA(1 DOWNTO 0) <= "00"; -- pad right side with 0s
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 14) <= sounddata(11)&sounddata(11); -- sign extend
+						R_DATA(13 DOWNTO 2) <= sounddata(11 DOWNTO 0);
+						R_DATA(1 DOWNTO 0) <= "00"; -- pad right side with 0s
+						
+				WHEN 1 =>
+						L_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+						L_DATA(12 DOWNTO 1) <= sounddata;
+						L_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+						R_DATA(12 DOWNTO 1) <= sounddata;
+						R_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+				
+				WHEN -2 =>
+						L_DATA(15 DOWNTO 12) <= sounddata(11)&sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+						L_DATA(11 DOWNTO 0) <= sounddata(11 DOWNTO 0);
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 12) <= sounddata(11)&sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+						R_DATA(11 DOWNTO 0) <= sounddata(11 DOWNTO 0);
+		     
+				WHEN -4 =>
+				L_DATA(15 DOWNTO 11) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
+						L_DATA(10 DOWNTO 0) <= sounddata(10 DOWNTO 0);
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 11) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
+						R_DATA(10 DOWNTO 0) <= sounddata(10 DOWNTO 0);
+		     
+			  WHEN -8 =>
+						L_DATA(15 DOWNTO 10) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
+						L_DATA(9 DOWNTO 0) <= sounddata(10 DOWNTO 1);
+						
+						-- Right channel is the same.
+						R_DATA(15 DOWNTO 10) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
+						R_DATA(9 DOWNTO 0) <= sounddata(10 DOWNTO 1);
+			  
+				WHEN OTHERS =>
+					L_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+					L_DATA(12 DOWNTO 1) <= sounddata;
+					L_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+					
+					-- Right channel is the same.
+					R_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
+					R_DATA(12 DOWNTO 1) <= sounddata;
+					R_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+				END CASE;
+			END IF;
 	END PROCESS;
    
    -- process to perform DDS
@@ -91,82 +186,64 @@ BEGIN
    END PROCESS;
 
    -- process to latch command data from SCOMP
-   PROCESS(RESETN, CMD) BEGIN
+   PROCESS(RESETN, CS) BEGIN
       IF RESETN = '0' THEN
 				playing <= notplayingNote;
 				tuning_word <= "000000000000";
 			   
 		Else
-								playing <= playingNote;
+				playing <= playingNote;
 						tuning_word <= "101010001001";
-
-				CASE CMD(9 DOWNTO 7) IS
-					WHEN "100" => -- 8
-						L_DATA(15 DOWNTO 4) <= sounddata(11 DOWNTO 0);
-						L_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
+				CASE (volume_current) IS
+					WHEN volumeUpThree => -- Octave 2
+						scale_factor :=8;
 						
-						-- Right channel is the same.
-						R_DATA(15 DOWNTO 4) <= sounddata(11 DOWNTO 0);
-						R_DATA(3 DOWNTO 0) <= "0000"; -- pad right side with 0s
-				
-					WHEN "010" => -- 4
-						L_DATA(15) <= sounddata(11); -- sign extend
-						L_DATA(14 DOWNTO 3) <= sounddata(11 DOWNTO 0);
-						L_DATA(2 DOWNTO 0) <= "000"; -- pad right side with 0s
+					WHEN volumeUpTwo => -- Octave 2
+						scale_factor :=4;
 						
-						-- Right channel is the same.
-						R_DATA(15) <= sounddata(11); -- sign extend
-						R_DATA(14 DOWNTO 3) <= sounddata(11 DOWNTO 0);
-						R_DATA(2 DOWNTO 0) <= "000"; -- pad right side with 0s
+					WHEN volumeUpOne => -- Octave 2
+						scale_factor :=2;
 						
-					WHEN "001" => -- 2
-						L_DATA(15 DOWNTO 14) <= sounddata(11)&sounddata(11); -- sign extend
-						L_DATA(13 DOWNTO 2) <= sounddata(11 DOWNTO 0);
-						L_DATA(1 DOWNTO 0) <= "00"; -- pad right side with 0s
+					WHEN default =>
+						scale_factor :=1;	
 						
-						-- Right channel is the same.
-						R_DATA(15 DOWNTO 14) <= sounddata(11)&sounddata(11); -- sign extend
-						R_DATA(13 DOWNTO 2) <= sounddata(11 DOWNTO 0);
-						R_DATA(1 DOWNTO 0) <= "00"; -- pad right side with 0s
-						
-					WHEN "110" => -- -2
-						L_DATA(15 DOWNTO 12) <= sounddata(11)&sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
-						L_DATA(11 DOWNTO 0) <= sounddata(11 DOWNTO 0);
-						
-						-- Right channel is the same.
-						R_DATA(15 DOWNTO 12) <= sounddata(11)&sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
-						R_DATA(11 DOWNTO 0) <= sounddata(11 DOWNTO 0);
-						
-					WHEN "101" => -- -4
+					WHEN volumeDownOne => -- Octave 2
+						scale_factor :=-2;
 					
-						L_DATA(15 DOWNTO 11) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
-						L_DATA(10 DOWNTO 0) <= sounddata(10 DOWNTO 0);
+					WHEN volumeDownTwo => -- Octave 2
+						scale_factor :=-4;
 						
-						-- Right channel is the same.
-						R_DATA(15 DOWNTO 11) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
-						R_DATA(10 DOWNTO 0) <= sounddata(10 DOWNTO 0);
-						
-					WHEN "011" => -- -8
-					
-						L_DATA(15 DOWNTO 10) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
-						L_DATA(9 DOWNTO 0) <= sounddata(10 DOWNTO 1);
-						
-						-- Right channel is the same.
-						R_DATA(15 DOWNTO 10) <= sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10)&sounddata(10); -- sign extend
-						R_DATA(9 DOWNTO 0) <= sounddata(10 DOWNTO 1);
+					WHEN volumeDownThree => -- Octave 2
+						scale_factor :=-8;
 						
 					WHEN OTHERS =>
-				
-						L_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
-					L_DATA(12 DOWNTO 1) <= sounddata;
-					L_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
-					
-					-- Right channel is the same.
-					R_DATA(15 DOWNTO 13) <= sounddata(11)&sounddata(11)&sounddata(11); -- sign extend
-					R_DATA(12 DOWNTO 1) <= sounddata;
-					R_DATA(0 DOWNTO 0) <= "0"; -- pad right side with 0s
+						scale_factor :=1;						
 					END case;
       END IF;
-		
    END PROCESS;
+	
+	PROCESS(KEY2) BEGIN
+      
+		IF FALLING_EDGE(KEY2) THEN
+			CASE volume_current IS
+				WHEN volumeUpThree =>
+					volume_current <= volumeDownThree;
+				WHEN volumeUpTwo =>
+					volume_current <= volumeUpThree;
+				WHEN volumeUpOne =>
+					volume_current <= volumeUpTwo;
+				WHEN default =>
+					volume_current <= volumeUpOne;
+				WHEN volumeDownOne =>
+					volume_current <= default;
+				WHEN volumeDownTwo =>
+					volume_current <= volumeDownOne;
+				WHEN volumeDownThree =>
+					volume_current <= volumeDownTwo;
+			END CASE;
+      END IF;
+   END PROCESS;
+	
+	
+
 END gen;
